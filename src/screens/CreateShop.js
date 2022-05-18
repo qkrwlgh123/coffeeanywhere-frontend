53c915e3b9cb55b497eb8ca30aca92838671771c
qkrwlgh123 <jiho1024@naver.com>
2022-05-18 14:54:41 +0900

    사진 여러개 업로드할 수 있도록 수정

--- src/screens/CreateShop.js
+++ src/screens/CreateShop.js
@@ -1,6 +1,6 @@
 import PageTitle from '../components/PageTitle';
 import styled from 'styled-components';
-import Input from '../components/auth/input';
+
 import { useState } from 'react';
 import { useEffect } from 'react';
 import Button from '../components/auth/Button';
@@ -13,7 +13,11 @@ import Notification from '../components/auth/Notification';
 import CreateLayout from '../components/shop/CreateLayout';
 import CreateShopBox from '../components/shop/CreateShopBox';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot, faX } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCamera,
+  faLocationDot,
+  faX,
+} from '@fortawesome/free-solid-svg-icons';
 import { Message } from './ShopInfo';
 
 const { kakao } = window;
@@ -133,32 +137,50 @@ const InputHashtags = styled.div`
 export const UploadBox = styled.div`
   display: flex;
   width: 50%;
+  justify-content: center;
   label {
     color: rgb(38, 38, 38);
     border: 0.5px solid rgba(219, 219, 219, 1);
     cursor: pointer;
-    margin-left: 5px;
     font-size: 15px;
     padding: 10px;
-    width: 40%;
+    width: 100px;
+    height: 100px;
+    margin-bottom: 10px;
     display: flex;
     align-items: center;
     justify-content: center;
     border-radius: 5px;
+    span {
+      margin-left: 5px;
+    }
   }
 `;
 
-const UploadName = styled.input`
-  width: 100%;
-  border-radius: 5px;
-  padding: 10px;
-  background-color: #fafafa;
-  border: 0.5px solid rgb(219, 219, 219);
-  box-sizing: border-box;
-  font-size: 18px;
-  &::placeholder {
-    font-size: 18px;
+const PhotoPrevBox = styled.div`
+  display: flex;
+  img {
+    margin: 0px 5px;
   }
+`;
+
+const FirstPrevPhoto = styled.div`
+  img {
+    margin: 0px;
+  }
+  div {
+    text-align: center;
+    background-color: black;
+    opacity: 0.8;
+    color: white;
+    width: 150px;
+    margin-top: -32px;
+    padding: 8px;
+  }
+`;
+
+const PrevPhotoLength = styled.span`
+  color: ${(props) => (props.length > 0 ? 'orange' : 'black')};
 `;
 
 export const FileUpload = styled.input`
@@ -177,7 +199,7 @@ const CREATE_SHOP_MUTATION = gql`
     $longitude: String
     $latitude: String
     $description: String
-    $file: Upload
+    $file: [Upload]
   ) {
     createShop(
       name: $name
@@ -195,16 +217,20 @@ const CREATE_SHOP_MUTATION = gql`
 
 function CreateShop() {
   const navigate = useNavigate();
-  const [fileName, setFileName] = useState('');
+
   const [hashTagNum, setHashTagNum] = useState(1);
   const [hashTagArr, setHashTagArr] = useState([hashTagNum]);
   const [maxhashTag, setMaxhashTag] = useState('');
+
+  const [photoPrevArr, setPhotoPrevArr] = useState([]);
+  const [photoUploadArr, setPhotoUploadArr] = useState([]);
+
   const handleHashtagNum = () => {
     if (hashTagNum < 5) {
       setHashTagNum((current) => current + 1);
       setHashTagArr([...hashTagArr, hashTagNum]);
     } else {
-      setMaxhashTag('최대 5개까지 추가 가능합니다.');
+      setMaxhashTag('해쉬태그는 최대 5개까지 추가 가능합니다');
     }
   };
 
@@ -280,7 +306,18 @@ function CreateShop() {
     setShopName('');
   };
 
-  const handleFileName = (event) => setFileName(event.target.files[0].name);
+  const handleAddFile = (event) => {
+    const reader = new FileReader();
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+    }
+    reader.onloadend = () => {
+      const base64Src = reader.result;
+      const imgFile = event.target.files[0];
+      setPhotoPrevArr([...photoPrevArr, base64Src]);
+      setPhotoUploadArr([...photoUploadArr, imgFile]);
+    };
+  };
 
   const [createShop, { loading }] = useMutation(CREATE_SHOP_MUTATION, {
     onCompleted,
@@ -298,11 +335,16 @@ function CreateShop() {
     let caption = '';
     for (let i = 1; i <= 5; i++) {
       if (data[i] !== undefined) {
+        if (caption.includes(data[i]) && data[i] !== '') {
+          alert('중복된 해쉬태그가 존재합니다.');
+          return;
+        }
         caption = caption + `#${data[i]}`;
       }
     }
-    const { file, description } = data;
+    const { description } = data;
     const { name, latitude, longitude } = sendData;
+
     createShop({
       variables: {
         name,
@@ -310,7 +352,7 @@ function CreateShop() {
         longitude,
         latitude,
         description,
-        file: file[0],
+        file: photoUploadArr,
       },
     });
   };
@@ -376,7 +418,6 @@ function CreateShop() {
                 />
               </SelectedShop>
             )}
-
             <InputHashtagsBox>
               {hashTagArr.map((item, index) => (
                 <InputHashtags key={index}>
@@ -385,7 +426,7 @@ function CreateShop() {
                     ref={register}
                     name={`${index + 1}`}
                     type="text"
-                    placeholder="해쉬 태그"
+                    placeholder="해쉬 태그 입력 "
                   />
                 </InputHashtags>
               ))}
@@ -393,20 +434,45 @@ function CreateShop() {
               <span onClick={handleHashtagNum}>해쉬태그 추가</span>
             </InputHashtagsBox>
             <UploadBox>
-              <UploadName
-                disabled
-                placeholder={fileName === '' ? '사진 첨부' : fileName}
-              />
-              <label htmlFor="file">파일 찾기</label>
+              <label htmlFor="file">
+                <FontAwesomeIcon icon={faCamera} color="black" size="1x" />
+                <span>
+                  <PrevPhotoLength length={photoPrevArr.length}>
+                    {photoPrevArr.length}
+                  </PrevPhotoLength>
+                  /4
+                </span>
+              </label>
               <FileUpload
                 id="file"
-                onChange={handleFileName}
                 ref={register}
+                onChange={handleAddFile}
                 name="file"
                 type="file"
                 accept="image/*"
+                disabled={photoPrevArr.length >= 4}
               />
             </UploadBox>
+            <PhotoPrevBox>
+              {photoPrevArr.map((item, index) =>
+                index === 0 ? (
+                  <FirstPrevPhoto>
+                    <img
+                      src={item}
+                      style={{ width: '150px', height: '150px' }}
+                    />
+                    <div>대표 사진</div>
+                  </FirstPrevPhoto>
+                ) : (
+                  <div>
+                    <img
+                      src={item}
+                      style={{ width: '150px', height: '150px' }}
+                    />
+                  </div>
+                )
+              )}
+            </PhotoPrevBox>
             <DescribeInput
               ref={register}
               name="description"
